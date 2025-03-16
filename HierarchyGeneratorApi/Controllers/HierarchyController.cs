dabbd7dafa085cf45d72d65d8668d3c0eaa986ca
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-03-16 15:55:35 +0100

    #17 Extract API logic from Program.cs into dedicated Controllers

---
+++ HierarchyGeneratorApi/Controllers/HierarchyController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HierarchyGeneratorApi.Controllers;
+
+public class HierarchyController : ControllerBase
+{
+    [HttpGet("/api/hierarchy")]
+    public ActionResult<IEnumerable<Hierarchy>> GetHierarchies()
+    {
+        List<Hierarchy> hierarchies = new List<Hierarchy>();
+        Hierarchy h1 = new Hierarchy(1, "First Hierarchy", 5, DateTime.Now, DateTime.Now);
+        Hierarchy h2 = new Hierarchy(2, "Second Hierarchy", 7, DateTime.Now, DateTime.Now);
+        Hierarchy h3 = new Hierarchy(3, "Third Hierarchy", 9, DateTime.Now, DateTime.Now);
+        Hierarchy h4 = new Hierarchy(4, "Fourth Hierarchy", 3, DateTime.Now, DateTime.Now);
+        Hierarchy h5 = new Hierarchy(5, "Fifths Hierarchy", 1, DateTime.Now, DateTime.Now);
+        hierarchies.Add(h1);
+        hierarchies.Add(h2);
+        hierarchies.Add(h3);
+        hierarchies.Add(h4);
+        hierarchies.Add(h5);
+        return hierarchies;
+    }
+    public record Hierarchy(int Id, string Name, int NumberOfNodes, DateTime CreatedDate, DateTime LastModified)
+    {
+
+    }
+
+}
+
+
+
--- HierarchyGeneratorApi/Program.cs
+++ HierarchyGeneratorApi/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -16,27 +17,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapGet("/api/hierarchies", () =>
-{
-    List<Hierarchy> hierarchies = new List<Hierarchy>();
-    Hierarchy h1 = new Hierarchy(1, "FirstHierarchy", 5, DateTime.Now,DateTime.Now);
-    Hierarchy h2 = new Hierarchy(2, "SecondHierarchy", 7, DateTime.Now, DateTime.Now);
-    Hierarchy h3 = new Hierarchy(3, "ThirdHierarchy", 9, DateTime.Now, DateTime.Now);
-    Hierarchy h4 = new Hierarchy(4, "FourthHierarchy", 3, DateTime.Now, DateTime.Now);
-    Hierarchy h5 = new Hierarchy(5, "FifthsHierarchy", 1, DateTime.Now, DateTime.Now);
-    hierarchies.Add(h1);
-    hierarchies.Add(h2);
-    hierarchies.Add(h3);
-    hierarchies.Add(h4);
-    hierarchies.Add(h5);
-    return hierarchies;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
+
+app.MapControllers();
 app.Run();
 
-record Hierarchy(int Id, string Name, int NumberOfNodes, DateTime CreatedDate, DateTime LastModified)
-{
-    
-}
+
