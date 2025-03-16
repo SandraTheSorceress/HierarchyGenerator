11480e7a60526027958a27dcc29d241421ca21fe
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-03-16 16:49:38 +0100

    #17 Introduce a Repository Layer to manage database interactions.

--- HierarchyGeneratorApi/Program.cs
+++ HierarchyGeneratorApi/Program.cs
@@ -1,3 +1,4 @@
+using HierarchyGeneratorApi.Repositories;
 using HierarchyGeneratorApi.Services;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IHierarchyService, HierarchyService>();
+builder.Services.AddScoped<IHierarchyRepository, HierarchyRepository>();
 
 var app = builder.Build();
 
--- HierarchyGeneratorApi/Services/HierarchyService.cs
+++ HierarchyGeneratorApi/Services/HierarchyService.cs
@@ -1,18 +1,18 @@
 ﻿using HierarchyGeneratorApi.Models;
+using HierarchyGeneratorApi.Repositories;
 
 namespace HierarchyGeneratorApi.Services;
 
 public class HierarchyService : IHierarchyService
 {
+    private readonly IHierarchyRepository _hierarchyRepository;
+
+    public HierarchyService(IHierarchyRepository hierarchyRepository)
+    {
+        _hierarchyRepository = hierarchyRepository;
+    }
     public List<Hierarchy> GetHierarchies()
     {
-        List<Hierarchy> hierarchies = new List<Hierarchy>();
-        Hierarchy h1 = new Hierarchy() { Id = 1, Name = "First Hierarchy", NumberOfNodes = 1, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
-        Hierarchy h2 = new Hierarchy() { Id = 2, Name = "Second Hierarchy", NumberOfNodes = 7, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
-        Hierarchy h3 = new Hierarchy() { Id = 3, Name = "Third Hierarchy", NumberOfNodes = 9, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
-        hierarchies.Add(h1);
-        hierarchies.Add(h2);
-        hierarchies.Add(h3);
-        return hierarchies;
+        return _hierarchyRepository.GetHierarchies();
     }
 }
