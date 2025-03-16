7668339dec3392072212a79685586d310b339f13
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-03-16 16:37:26 +0100

    #17 added service layer

--- HierarchyGeneratorApi/Controllers/HierarchyController.cs
+++ HierarchyGeneratorApi/Controllers/HierarchyController.cs
@@ -1,21 +1,22 @@
 ﻿using HierarchyGeneratorApi.Models;
+using HierarchyGeneratorApi.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HierarchyGeneratorApi.Controllers;
 
 public class HierarchyController : ControllerBase
 {
+    private readonly IHierarchyService _hierarchyService;
+
+    public HierarchyController(IHierarchyService hierarchyService)
+    {
+        _hierarchyService = hierarchyService;
+    }
+
     [HttpGet("/api/hierarchy")]
     public ActionResult<IEnumerable<Hierarchy>> GetHierarchies()
     {
-        List<Hierarchy> hierarchies = new List<Hierarchy>();
-        Hierarchy h1 = new Hierarchy() { Id=1, Name="First Hierarchy", NumberOfNodes=5, CreatedDate=DateTime.Now, LastModified=DateTime.Now };
-        Hierarchy h2 = new Hierarchy() { Id = 2, Name = "Second Hierarchy", NumberOfNodes = 7, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
-        Hierarchy h3 = new Hierarchy() { Id = 3, Name = "Third Hierarchy", NumberOfNodes = 9, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
-        hierarchies.Add(h1);
-        hierarchies.Add(h2);
-        hierarchies.Add(h3);
-        return hierarchies;
+        return _hierarchyService.GetHierarchies();
     }
    
 
---
+++ HierarchyGeneratorApi/Services/HierarchyService.cs
@@ -0,0 +1,18 @@
+﻿using HierarchyGeneratorApi.Models;
+
+namespace HierarchyGeneratorApi.Services;
+
+public class HierarchyService : IHierarchyService
+{
+    public List<Hierarchy> GetHierarchies()
+    {
+        List<Hierarchy> hierarchies = new List<Hierarchy>();
+        Hierarchy h1 = new Hierarchy() { Id = 1, Name = "First Hierarchy", NumberOfNodes = 1, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
+        Hierarchy h2 = new Hierarchy() { Id = 2, Name = "Second Hierarchy", NumberOfNodes = 7, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
+        Hierarchy h3 = new Hierarchy() { Id = 3, Name = "Third Hierarchy", NumberOfNodes = 9, CreatedDate = DateTime.Now, LastModified = DateTime.Now };
+        hierarchies.Add(h1);
+        hierarchies.Add(h2);
+        hierarchies.Add(h3);
+        return hierarchies;
+    }
+}
