7e4d92bab0e985c638de9b36d4a3ac1ddec49d34
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-03-22 18:57:16 +0100

    #7 Handled displaying errors to the user when calling backend API throws an error

--- HierarchyGeneratorClient/src/App.jsx
+++ HierarchyGeneratorClient/src/App.jsx
@@ -4,21 +4,30 @@ import Header from "./Header";
 import HierarchyOverview from "./HierarchyOverview";
 
 function App() {
-
   const [hierarchies, setHierarchies] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("/api/hierarchy")
       .then((response) => response.json())
-      .then((data) => setHierarchies(data));
+      .then((data) => setHierarchies(data))
+      .catch((err) => {
+        setError(
+          "⚠️ Something went wrong fetching the hierarchy. Please try again later."
+        );
+      });
   }, []);
 
   return (
     <>
       <Header title="Hierarchy Generator" />
-      <HierarchyOverview hierarchyList={hierarchies} />
+      {!error ? (
+        <HierarchyOverview hierarchyList={hierarchies} />
+      ) : (
+        <p style={{ color: "red" }}>{error}</p>
+      )}
     </>
   );
-};
+}
 
 export default App;
