675401a78e7db98da57f14be25049545c23e72f8
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-03-22 12:32:44 +0100

    #4 Updated database.sql The script was partially inspired by migration scripts created with Entity Framework Tools using commands:
     dotnet ef migrations add InitialMigration
    
     dotnet ef migrations script -f -o migration.sql

--- HierarchyGeneratorDatabase/database.sql
+++ HierarchyGeneratorDatabase/database.sql
@@ -1,18 +1,19 @@
-	CREATE DATABASE hierarchy_generator;
+CREATE DATABASE hierarchy_generator;
 
-	use hierarchy_generator;
+use hierarchy_generator;
 
-	create table hierarchies (
-	id int PRIMARY KEY,
-	name text,
-	numberOfNodes int,
-	createdDate DATETIME,
-	lastModified DATETIME
-	);
+CREATE TABLE Hierarchies (
+    Id int NOT NULL AUTO_INCREMENT,
+    Name longtext CHARACTER SET utf8mb4 NOT NULL,
+    NumberOfNodes int NOT NULL,
+    CreatedDate datetime NOT NULL,
+    LastModified datetime NOT NULL,
+    CONSTRAINT PK_Hierarchies PRIMARY KEY (Id)
+) CHARACTER SET=utf8mb4;
 
-	insert into hierarchies values (1, "Hierarchy 1", 1, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
-	insert into hierarchies values (2, "Hierarchy 2",5, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
-	insert into hierarchies values (3, "Hierarchy 3",7, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
-	insert into hierarchies values (4, "Hierarchy 4", 9, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
+insert into hierarchies (Name, NumberOfNodes, CreatedDate, LastModified) values ("Hierarchy 1", 1, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
+insert into hierarchies (Name, NumberOfNodes, CreatedDate, LastModified) values ("Hierarchy 2", 5, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
+insert into hierarchies (Name, NumberOfNodes, CreatedDate, LastModified) values ("Hierarchy 3", 7, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
+insert into hierarchies (Name, NumberOfNodes, CreatedDate, LastModified) values ("Hierarchy 4", 9, '2025-03-22 14:30:00', '2025-03-22 14:30:00');
 
-	commit;
+commit;
