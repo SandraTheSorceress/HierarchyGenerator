3b264dac925e3122dd1371f9e7416698e96ae52d
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-05 20:15:16 +0200

    #36 Created a new endpoint GET /api/random/name that prompts the LLM for a random name. Added package Microsoft.Extensions.AI.Ollama Created a RandomService that connects to Ollama running in docker.

---
+++ HierarchyGeneratorApi/Services/RandomService.cs
@@ -0,0 +1,19 @@
+﻿using HierarchyGeneratorApi.Models;
+using HierarchyGeneratorApi.Repositories;
+using Microsoft.Extensions.AI;
+using Serilog;
+
+namespace HierarchyGeneratorApi.Services;
+
+public class RandomService : IRandomService
+{
+
+    
+    public string GetRandomName()
+    {
+        IChatClient chatClient = new OllamaChatClient(new Uri("http://localhost:11434/"), "phi3:mini");
+        var prompt = "Give me a fictional first and last name! Respond with only the full name, nothing else.";
+        var response = chatClient.GetResponseAsync(prompt, cancellationToken: default).ConfigureAwait(false).GetAwaiter().GetResult();
+        return response.Text;
+    }
+}
