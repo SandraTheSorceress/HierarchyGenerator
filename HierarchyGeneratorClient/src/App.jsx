686d0b4e911da4ffca5b022a3c43006ad55d256d
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-06 10:40:33 +0200

    #38 Added a Pacman spinner using the npm package react-spinners. This shows when data is being fetched from backend.

--- HierarchyGeneratorClient/src/App.jsx
+++ HierarchyGeneratorClient/src/App.jsx
@@ -1,31 +1,47 @@
 import { useEffect, useState } from "react";
 import Header from "./Header";
 import HierarchyOverview from "./HierarchyOverview";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 function App() {
   const [hierarchies, setHierarchies] = useState([]);
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("/backend/api/hierarchy")
       .then((response) => response.json())
-      .then((data) => setHierarchies(data))
+      .then((data) => {
+        setHierarchies(data);
+        setLoading(false);
+      })
       .catch((err) => {
         setError(
           "⚠️ Something went wrong fetching the hierarchy. Please try again later."
         );
+        setLoading(false);
       });
   }, []);
 
   return (
     <div>
       <Header title="Hierarchy Generator" />
-      {!error ? (
+
+      {loading ? (
+        <div className="flex items-center justify-center pt-7">
+          <PacmanLoader
+            color="#642392"
+            size={150}
+            aria-label="Loading Spinner"
+            data-testid="loader"
+          />
+        </div>
+      ) : error ? (
+        <p style={{ color: "red" }}>{error}</p>
+      ) : (
         <div className="flex items-center justify-center pt-7">
           <HierarchyOverview hierarchyList={hierarchies} />
         </div>
-      ) : (
-        <p style={{ color: "red" }}>{error}</p>
       )}
     </div>
   );
