22c0ae0b724d479118c01202893d0b7fdc854f87
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-06 16:52:47 +0200

    #43 Removed next and previous buttons

--- HierarchyGeneratorClient/src/HierarchyOverview.jsx
+++ HierarchyGeneratorClient/src/HierarchyOverview.jsx
@@ -152,7 +152,7 @@ function HierarchyOverview({ hierarchyList, setSearchQuery, setPage }) {
           </tbody>
         </table>
         <nav
-          className="flex items-center flex-column flex-wrap md:flex-row justify-between pt-4"
+          className="flex items-center flex-column flex-wrap md:flex-row justify-between p-4"
           aria-label="Table navigation"
         >
           <span className="text-sm font-normal text-gray-500 dark:text-gray-400 mb-4 md:mb-0 block w-full md:inline md:w-auto">
@@ -168,23 +168,7 @@ function HierarchyOverview({ hierarchyList, setSearchQuery, setPage }) {
             hierarchies
           </span>
           <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Previous
-              </a>
-            </li>
             { page_buttons }
-            <li>
-              <a
-                href="#"
-                className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-              >
-                Next
-              </a>
-            </li>
           </ul>
         </nav>
       </div>
