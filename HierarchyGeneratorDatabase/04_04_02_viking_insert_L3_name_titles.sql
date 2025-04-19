8e07ce4e1028c04c45df1f711b3079223fe0e988
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-19 19:15:53 +0200

    Added data to create L3 in space and viking theme

---
+++ HierarchyGeneratorDatabase/04_04_02_viking_insert_L3_name_titles.sql
@@ -0,0 +1,8 @@
+use hierarchy_generator;
+
+INSERT INTO `NameTitles` (`Theme`, `Category`, `NodeLevel`, `Value`) VALUES ('VIKING', 'PLACE', 'L3', 'longhouse of');
+INSERT INTO `NameTitles` (`Theme`, `Category`, `NodeLevel`, `Value`) VALUES ('VIKING', 'PLACE', 'L3', 'hearth of');
+INSERT INTO `NameTitles` (`Theme`, `Category`, `NodeLevel`, `Value`) VALUES ('VIKING', 'PLACE', 'L3', 'stead of');
+INSERT INTO `NameTitles` (`Theme`, `Category`, `NodeLevel`, `Value`) VALUES ('VIKING', 'PLACE', 'L3', 'encampment of');
+
+commit;
