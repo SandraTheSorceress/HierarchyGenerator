26e7110750cc535a954c9d0665293f058c8d9475
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-19 11:14:10 +0200

    #75 Extracted SearchBar out from HierarchyOverview.jsx

--- HierarchyGeneratorClient/src/components/HierarchyOverview.jsx
+++ HierarchyGeneratorClient/src/components/HierarchyOverview.jsx
@@ -1,5 +1,6 @@
 import { daysAgo, calculateRangeStart, calculateRangeEnd } from "../utils/utils";
 import Pagination from "./Pagination";
+import SearchBar from "./SearchBar";
 
 function deleteHierarchy(hierarchy, setMessage, refreshPage) {
   fetch(`/backend/api/hierarchy/${hierarchy.id}`, {
@@ -25,45 +26,11 @@ function deleteHierarchy(hierarchy, setMessage, refreshPage) {
 function HierarchyOverview({ hierarchyList, setSearchQuery, setPage, refreshPage, setMessage }) {
   return (
     <div className="bg-gray-50 dark:bg-gray-800 relative shadow-md sm:rounded-lg overflow-hidden mt-10">
-      <div className="flex flex-col md:flex-row items-center justify-between space-y-3 md:space-y-0 md:space-x-4 p-4">
-        <div className="w-full md:w-1/2">
-          <form className="flex items-center">
-            <label htmlFor="simple-search" className="sr-only">
-              Search
-            </label>
-            <div className="relative w-full">
-              <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-                <svg
-                  aria-hidden="true"
-                  className="w-5 h-5 text-gray-500 dark:text-gray-400"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </div>
-              <input
-                type="text"
-                id="simple-search"
-                className="bg-gray-50 border border-gray-300 text-gray-900 
-                text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 
-                block w-2/5 pl-10 p-2 dark:bg-gray-700 dark:border-gray-600
-                 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                placeholder="Search"
-                required=""
-                onChange={(event) => {
-                  setPage(1)
-                  setSearchQuery(event.target.value)
-                }}
-              />
-            </div>
-          </form>
-        </div>
+      <div className="p-4">
+      <SearchBar onSearch={(query) => {
+          setPage(1);
+          setSearchQuery(query);
+        }} />
       </div>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg px-2">
