781253c9ed6cbb61a46372872c06f49c808bf920
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-04-27 12:28:16 +0200

    #82 added accordian to hierarchy row, and included lucide-react library to display chevron icon

--- HierarchyGeneratorClient/src/components/HierarchyRow.jsx
+++ HierarchyGeneratorClient/src/components/HierarchyRow.jsx
@@ -1,30 +1,48 @@
+import { useState } from "react";
 import { daysAgo } from "../utils/utils";
+import { ChevronRight, ChevronDown } from "lucide-react";
 
 function HierarchyRow({ hierarchy, userInfo, onDeleteClick }) {
-    return (
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <>
       <tr
         key={hierarchy.id}
         className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
       >
         <th
           scope="row"
-          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white min-w-[40rem]"
+          className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white min-w-[40rem] flex items-center gap-2 cursor-pointer"
+          onClick={() => setIsExpanded(!isExpanded)}
         >
+          {/* Arrow Icon */}
+          {isExpanded ? (
+            <ChevronDown size={24} />
+          ) : (
+            <ChevronRight size={24} />
+          )}
           {hierarchy.name}
         </th>
-        <td className="px-6 py-4">{daysAgo(hierarchy.createdDate)}</td>
-  
+
+        <td className="px-6 py-4 cursor-pointer"
+        onClick={() => setIsExpanded(!isExpanded)}
+        >
+            {daysAgo(hierarchy.createdDate)}</td>
+
         <td className="px-6 py-4 text-right">
           {userInfo && (
             <button
               className="min-w-[90px] inline-block px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-700 transition-colors"
-              onClick={() => onDeleteClick(hierarchy)}
+              onClick={(e) => {
+                onDeleteClick(hierarchy);
+              }}
             >
               Delete
             </button>
           )}
         </td>
-  
+
         <td className="px-6 py-4 text-right">
           <a
             href={`/backend/api/hierarchy/${hierarchy.id}/download`}
@@ -34,8 +52,20 @@ function HierarchyRow({ hierarchy, userInfo, onDeleteClick }) {
           </a>
         </td>
       </tr>
-    );
-  }
-  
-  export default HierarchyRow;
-  
+
+      {isExpanded && (
+        <tr className="bg-gray-100 dark:bg-gray-800">
+          <td colSpan="4" className="px-6 py-4 text-gray-700 dark:text-gray-300">
+            <div className="ml-16">
+              <p><strong>Nodes:</strong> {hierarchy.nodeCount}</p>
+              <p><strong>Contacts:</strong> {hierarchy.nodeCount}</p>
+              <p><strong>Attributes:</strong> 10</p>
+            </div>
+          </td>
+        </tr>
+      )}
+    </>
+  );
+}
+
+export default HierarchyRow;
