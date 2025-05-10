617755ef2387f096b4812675a2d32196c53ab6a6
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-05-10 14:12:03 +0200

    #114 Added a few more unit tests for CalculatePagination

--- HierarchyGeneratorApi.Test/ListMetaServiceTest.cs
+++ HierarchyGeneratorApi.Test/ListMetaServiceTest.cs
@@ -54,4 +54,55 @@ public class ListMetaServiceTest
         Assert.Equal(95, startingIndex);
         Assert.Equal(5, itemsOnPage);
     }
+
+    [Fact]
+    public void CalculatePagination_PageBeyondLastItemShouldReturnNoItemsOnPage()
+    {
+        // Given
+        int totalItems = 100;
+        int page = 21;
+        int limit = 5;
+
+        // When
+        IListMetaService listMetaService = new ListMetaService();
+        var (startingIndex, itemsOnPage) = listMetaService.CalculatePagination(totalItems, page, limit);
+
+        // Then
+        Assert.Equal(100, startingIndex);
+        Assert.Equal(0, itemsOnPage);
+    }
+
+    [Fact]
+    public void CalculatePagination_ShouldReturnRemainingItemsOnLastPage()
+    {
+        // Given
+        int totalItems = 7;
+        int page = 2;
+        int limit = 5;
+
+        // When
+        IListMetaService listMetaService = new ListMetaService();
+        var (startingIndex, itemsOnPage) = listMetaService.CalculatePagination(totalItems, page, limit);
+
+        // Then
+        Assert.Equal(5, startingIndex);
+        Assert.Equal(2, itemsOnPage);
+    }
+
+    [Fact]
+    public void CalculatePagination_ShouldReturnNoItems()
+    {
+        // Given
+        int totalItems = 0;
+        int page = 1;
+        int limit = 5;
+
+        // When
+        IListMetaService listMetaService = new ListMetaService();
+        var (startingIndex, itemsOnPage) = listMetaService.CalculatePagination(totalItems, page, limit);
+
+        // Then
+        Assert.Equal(0, startingIndex);
+        Assert.Equal(0, itemsOnPage);
+    }
 }
