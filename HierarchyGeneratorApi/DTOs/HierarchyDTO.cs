ae344a7f77cb3a84c5b5e9e296180da5b4757a01
Alexandra Isaksson <alexadra.isaksson@outlook.com>
2025-05-10 12:03:59 +0200

    #107 Added CreatedbyName and Email to GET /api/hiearchies

--- HierarchyGeneratorApi/DTOs/HierarchyDTO.cs
+++ HierarchyGeneratorApi/DTOs/HierarchyDTO.cs
@@ -4,6 +4,8 @@ public class HierarchyDTO
 {
     public int Id { get; set; }
     public string Name { get; set; }
+    public string CreatedByName { get; set; }
+    public string CreatedByEmail { get; set; }
     public int? NumberOfNodes { get; set; }
     public int? NumberOfContacts { get; set; }
     public DateTime CreatedDate { get; set; }
--- HierarchyGeneratorApi/Models/Hierarchy.cs
+++ HierarchyGeneratorApi/Models/Hierarchy.cs
@@ -4,9 +4,7 @@ public class Hierarchy
 {
     public int Id { get; set; }
     public string Name { get; set; }
-
     public string CreatedByName { get; set; }
-
     public string CreatedByEmail { get; set; }
     public DateTime CreatedDate { get; set; }
     public DateTime LastModified { get; set; }
